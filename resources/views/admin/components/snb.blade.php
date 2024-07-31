9e267dd99e7ff63b346f9a49f21e430e45493d3b
BaekJinYoung <time121412@gmail.com>
2024-07-31 17:52:01 +0900

    [admin] snb

--- resources/views/admin/components/snb.blade.php
+++ resources/views/admin/components/snb.blade.php
@@ -9,64 +9,43 @@
                     <i class="xi-angle-down-thin"></i>
                 </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.popupIndex")}}" class="sub-gnb-item">--}}
+                    <a href="{{route("admin.popupIndex")}}" class="sub-gnb-item">
                         메인 팝업
                     </a>
                 </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.bannerIndex")}}" class="sub-gnb-item">--}}
+                    <a href="{{route("admin.bannerIndex")}}" class="sub-gnb-item">
                         메인 배너
                     </a>
                 </div>
-            </div>
-            <div class="gnb-item">
-                <div class="item-default">
-                    회사소개
-                    <i class="xi-angle-down-thin"></i>
-                </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.historyIndex")}}" class="sub-gnb-item">--}}
-                        우수조달컨설팅 소개
-                    </a>
-                </div>
-                <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.companyIndex")}}" class="sub-gnb-item">--}}
-                        인증성공업체
-                    </a>
-                </div>
-                <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.youtubeIndex")}}" class="sub-gnb-item">--}}
-                        유튜브 소개
-                    </a>
-                </div>
-                <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.consultantIndex")}}" class="sub-gnb-item">--}}
-                        컨설턴트 소개
+                    <a href="{{route("admin.youtubeIndex")}}" class="sub-gnb-item">
+                        메인 유튜브
                     </a>
                 </div>
             </div>
             <div class="gnb-item">
                 <div class="item-default">
-                    고객지원
+                    고객후기
                     <i class="xi-angle-down-thin"></i>
                 </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.announcementIndex")}}" class="sub-gnb-item">--}}
+                    <a href="{{route("admin.announcementIndex")}}" class="sub-gnb-item">
                         공지사항
                     </a>
                 </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.shareIndex")}}" class="sub-gnb-item">--}}
-                        정보공유
-                    </a>
-                </div>
-                <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.questionIndex")}}" class="sub-gnb-item">--}}
+                    <a href="{{route("admin.questionIndex")}}" class="sub-gnb-item">
                         자주묻는 질문
                     </a>
                 </div>
                 <div class="sub-gnb row-group">
-{{--                    <a href="{{route("admin.inquiryIndex")}}" class="sub-gnb-item">--}}
+                    <a href="{{route("admin.reviewIndex")}}" class="sub-gnb-item">
+                        고객후기
+                    </a>
+                </div>
+                <div class="sub-gnb row-group">
+                    <a href="{{route("admin.inquiryIndex")}}" class="sub-gnb-item">
                         문의하기
                     </a>
                 </div>
