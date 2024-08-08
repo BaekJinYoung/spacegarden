98ee4965c2ff99b97285c67431b169da5214905e
BaekJinYoung <time121412@gmail.com>
2024-08-08 15:29:37 +0900

    [api, admin] 고객 후기 필터 컬럼 타입 변환

--- database/migrations/2024_08_08_062504_change_column_types_in_reviews_table.php
+++ database/migrations/2024_08_08_062504_change_column_types_in_reviews_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,8 +13,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('reviews', function (Blueprint $table) {
-            $table->Integer('filter_category')->change();
-            $table->Integer('filter_area')->change();
+            DB::statement('ALTER TABLE reviews MODIFY filter_category INT NOT NULL;');
+            DB::statement('ALTER TABLE reviews MODIFY filter_area INT NOT NULL;');
         });
     }
 
