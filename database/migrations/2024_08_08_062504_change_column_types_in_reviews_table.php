ee1a4415568bec122d593246912b2af5a438c89d
BaekJinYoung <time121412@gmail.com>
2024-08-08 15:25:54 +0900

    [api, admin] 고객 후기 필터 컬럼 타입 변환

--- database/migrations/2024_08_08_062504_change_column_types_in_reviews_table.php
+++ database/migrations/2024_08_08_062504_change_column_types_in_reviews_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // 외래 키 제약 조건을 비활성화 (필요한 경우)
-//        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-//
-//        // 테이블의 모든 데이터 삭제
-//        DB::table('reviews')->delete();
-//
-//        // 외래 키 제약 조건을 다시 활성화
-//        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->Integer('filter_category')->change();
+            $table->Integer('filter_area')->change();
+        });
     }
 
     /**
