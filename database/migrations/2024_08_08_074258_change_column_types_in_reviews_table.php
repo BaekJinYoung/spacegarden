78ceb5bb393798fdd8529c445bc81431e3b0795c
BaekJinYoung <time121412@gmail.com>
2024-08-08 17:27:28 +0900

    [database] 고객 후기 대표 사진 required

--- database/migrations/2024_08_08_074258_change_column_types_in_reviews_table.php
+++ database/migrations/2024_08_08_074258_change_column_types_in_reviews_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,8 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // Update existing NULL values
+        DB::table('reviews')->whereNull('image')->update(['image' => 'images/TEST_웹.jpg']);
+
         Schema::table('reviews', function (Blueprint $table) {
-            $table->string('image')->default("images/TEST_웹.jpg")->change(); // 대표사진(썸네일)
+            $table->string('image')->default('images/TEST_웹.jpg')->change(); // 대표사진(썸네일)
         });
     }
 
