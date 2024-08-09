054c3ab2fd3b037032347511764323f89e22f350
BaekJinYoung <time121412@gmail.com>
2024-08-09 13:03:44 +0900

    [api] 블로그 api json_decode 삭제

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,25 +14,23 @@ class BlogController extends Controller
         $clientId = 'zq6e8lRLTCQqCVh1UfGt';  // 네이버 애플리케이션의 Client ID
         $clientSecret = '9okmubdb5x';  // 네이버 애플리케이션의 Client Secret
 
-        $query = 'site:blog.naver.com/niceout86';
+        $query = 'niceout86';
 
-        $response = $client->request('GET', 'https://openapi.naver.com/v1/search/blog.xml', [
+        $response = $client->request('GET', 'https://openapi.naver.com/v1/search/blog', [
             'headers' => [
                 'X-Naver-Client-Id' => $clientId,
                 'X-Naver-Client-Secret' => $clientSecret,
             ],
             'query' => [
                 'query' => $query,
-                'display' => 10, // 가져올 게시물 수
+                'display' => 20, // 가져올 게시물 수
                 'start' => 1, // 시작 인덱스
                 'sort' => 'date', // 정렬 기준 (sim: 유사도, date: 날짜)
             ],
         ]);
 
-        $data = json_decode($response->getBody(), true);
+        $data = $response;
 
-        $posts = $data['items'];
-
-        return ApiResponse::success($posts);
+        return ApiResponse::success($data);
     }
 }
